7517f70451494b160c11b57e8b182e73a117d1b1
张俊峰 <496209553@qq.com>
2016-05-17 11:17:53 +0800

    Initial Commit

---
+++ MLVActivityIndicatorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLVActivityIndicatorView
+//
+//  Created by Melvyn on 5/17/16.
+//  Copyright © 2016 Melvyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLVActivityIndicatorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLVActivityIndicatorView
+//
+//  Created by Melvyn on 5/17/16.
+//  Copyright © 2016 Melvyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
